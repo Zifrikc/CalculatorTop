2dd2e5b6cc874806d8ed9572cbecaa505c8cb65c
Zifrikc <zuran210@gmail.com>
2025-07-14 02:03:36 -0500

    estilos actualizados botones flotantes

--- android/app/src/main/java/com/operatortop/com/MainActivity.java
+++ android/app/src/main/java/com/operatortop/com/MainActivity.java
@@ -1,5 +1,19 @@
 package com.operatortop.com;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
+    }
+}
